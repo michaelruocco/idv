34dc7cc6ebb37f4b1a702b5e290007e667efafa9
Michael Ruocco <michael.ruocco@hotmail.com>
2019-09-22 14:27:04 +0100

    fixes get verification context endpoint

--- src/main/java/uk/co/mruoc/idv/verificationcontext/rest/VerificationContextController.java
+++ src/main/java/uk/co/mruoc/idv/verificationcontext/rest/VerificationContextController.java
@@ -1,6 +1,7 @@
 package uk.co.mruoc.idv.verificationcontext.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import uk.co.mruoc.idv.verificationcontext.domain.service.VerificationContextSer
 
 import java.util.UUID;
 
-@RestController("/verificationContexts")
+@RestController
 public class VerificationContextController {
 
     private final VerificationContextService contextService;
@@ -21,22 +22,27 @@ public class VerificationContextController {
         this.contextService = contextService;
     }
 
-    @PostMapping
+    @PostMapping("/verificationContexts")
     public VerificationContextDocument createContext(@RequestBody final CreateContextRequestDocument request) {
         final VerificationContext context = contextService.create(request.getAttributes());
-        return new VerificationContextDocument(context);
+        return toDocument(context);
     }
 
-    @GetMapping("/{id}")
-    public VerificationContext getContext(final UUID id) {
+    @GetMapping("/verificationContexts/{id}")
+    public VerificationContextDocument getContext(@PathVariable("id") final UUID id) {
         final GetContextRequest request = toGetContextRequest(id);
-        return contextService.get(request);
+        final VerificationContext context = contextService.get(request);
+        return toDocument(context);
     }
 
     private static GetContextRequest toGetContextRequest(final UUID id) {
         return GetContextRequest.builder()
                 .id(id)
                 .build();
+    }
+
+    private static VerificationContextDocument toDocument(final VerificationContext context) {
+        return new VerificationContextDocument(context);
     }
 
 }
