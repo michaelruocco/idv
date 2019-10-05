b73755e9444c19a94477a72211ee8e8b52e40117
Michael Ruocco <michael.ruocco@hotmail.com>
2019-10-05 17:00:52 +0100

    adds tests for record on sequence complete

---
+++ src/test/java/uk/co/mruoc/idv/lockout/domain/service/RecordOnSequenceCompleteTest.java
@@ -0,0 +1,45 @@
+package uk.co.mruoc.idv.lockout.domain.service;
+
+import org.junit.jupiter.api.Test;
+import uk.co.mruoc.idv.verificationcontext.domain.model.VerificationContext;
+import uk.co.mruoc.idv.verificationcontext.domain.model.result.FakeVerificationResultSuccessful;
+import uk.co.mruoc.idv.verificationcontext.domain.model.result.VerificationResult;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+class RecordOnSequenceCompleteTest {
+
+    private final VerificationContext context = mock(VerificationContext.class);
+    private final VerificationResult result = new FakeVerificationResultSuccessful("method-name");
+
+    private final RecordAttemptStrategy strategy = new RecordOnSequenceComplete();
+
+    @Test
+    void shouldReturnFalseIfContextDoesNotContainCompleteSequenceContainingResultMethod() {
+        given(context.containsCompleteSequenceContainingMethod(result.getMethodName())).willReturn(false);
+        final RecordAttemptRequest request = RecordAttemptRequest.builder()
+                .context(context)
+                .result(result)
+                .build();
+
+        boolean result = strategy.shouldRecordAttempt(request);
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void shouldReturnTrueIfContextContainsCompleteSequenceContainingResultMethod() {
+        given(context.containsCompleteSequenceContainingMethod(result.getMethodName())).willReturn(true);
+        final RecordAttemptRequest request = RecordAttemptRequest.builder()
+                .context(context)
+                .result(result)
+                .build();
+
+        boolean result = strategy.shouldRecordAttempt(request);
+
+        assertThat(result).isTrue();
+    }
+
+}
