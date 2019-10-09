6e7e49d81d4933a6ecb005433b7ed64af229586b
Michael Ruocco <michael.ruocco@hotmail.com>
2019-10-09 06:10:39 +0100

    adds tests for default lockout request

---
+++ src/test/java/uk/co/mruoc/idv/lockout/domain/service/DefaultLockoutRequestTest.java
@@ -0,0 +1,50 @@
+package uk.co.mruoc.idv.lockout.domain.service;
+
+import org.junit.jupiter.api.Test;
+import uk.co.mruoc.idv.identity.domain.model.Alias;
+import uk.co.mruoc.idv.identity.domain.model.FakeCreditCardNumber;
+import uk.co.mruoc.idv.lockout.domain.service.DefaultLockoutRequest.DefaultLockoutRequestBuilder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DefaultLockoutRequestTest {
+
+    private final DefaultLockoutRequestBuilder builder = DefaultLockoutRequest.builder();
+
+    @Test
+    void shouldReturnChannelId() {
+        final String channelId = "channel-id";
+
+        final LockoutRequest request = builder.channelId(channelId).build();
+
+        assertThat(request.getChannelId()).isEqualTo(channelId);
+    }
+
+    @Test
+    void shouldReturnActivityName() {
+        final String activityName = "activity-name";
+
+        final LockoutRequest request = builder.activityName(activityName).build();
+
+        assertThat(request.getActivityName()).isEqualTo(activityName);
+    }
+
+    @Test
+    void shouldReturnAlias() {
+        final Alias alias = new FakeCreditCardNumber();
+
+        final LockoutRequest request = builder.alias(alias).build();
+
+        assertThat(request.getAlias()).isEqualTo(alias);
+    }
+
+    @Test
+    void shouldReturnAliasType() {
+        final Alias alias = new FakeCreditCardNumber();
+
+        final LockoutRequest request = builder.alias(alias).build();
+
+        assertThat(request.getAliasType()).isEqualTo(alias.getType());
+    }
+
+}
