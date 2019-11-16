24fc63ff4e0bcb2a716e937b24ee7f2abf95a512
Michael Ruocco <michael.ruocco@hotmail.com>
2019-11-16 11:35:18 +0000

    ensure empty optionals are not rendered as nulls when serializing classes to json

--- adapter/api/src/main/java/uk/co/idv/api/ObjectMapperSingleton.java
+++ adapter/api/src/main/java/uk/co/idv/api/ObjectMapperSingleton.java
@@ -1,5 +1,6 @@
 package uk.co.idv.api;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -34,6 +35,7 @@ public class ObjectMapperSingleton {
                 new JsonApiVerificationContextModule(),
                 new JsonApiLockoutStateModule()
         );
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_ABSENT);
         mapper.disable(WRITE_DATES_AS_TIMESTAMPS);
         return mapper;
     }
