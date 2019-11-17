3d77df69951400a3b323ad0cc5f4850d9c0144eb
Michael Ruocco <michael.ruocco@hotmail.com>
2019-11-17 11:18:32 +0000

    adds tests uk for api module

---
+++ geography/uk/api/src/test/java/uk.co.idv.uk.api.channel/UkChannelModuleTest.java
@@ -0,0 +1,35 @@
+package uk.co.idv.uk.api.channel;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import uk.co.idv.domain.entities.channel.Channel;
+import uk.co.idv.uk.domain.entities.channel.Rsa;
+import uk.co.mruoc.file.content.ContentLoader;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UkChannelModuleTest {
+
+    private static final ObjectMapper MAPPER = UkObjectMapperSingleton.get();
+
+    @Test
+    void shouldSerializeRsaChannelWithIssuerSessionId() throws IOException {
+        final String json = ContentLoader.loadContentFromClasspath("rsa/rsa-channel-with-issuer-session-id.json");
+
+        final Channel channel = MAPPER.readValue(json, Channel.class);
+
+        assertThat(channel).isEqualToComparingFieldByField(new Rsa("1234567890"));
+    }
+
+    @Test
+    void shouldSerializeRsaChannelWithoutIssuerSessionId() throws IOException {
+        final String json = ContentLoader.loadContentFromClasspath("rsa/rsa-channel-without-issuer-session-id.json");
+
+        final Channel channel = MAPPER.readValue(json, Channel.class);
+
+        assertThat(channel).isEqualToComparingFieldByField(new Rsa());
+    }
+
+}
