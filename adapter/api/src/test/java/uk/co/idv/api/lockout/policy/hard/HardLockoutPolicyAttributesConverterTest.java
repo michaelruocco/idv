26762cf9bc35c381b7e0043dc9b3ec8a44a892e7
Michael Ruocco <michael.ruocco@hotmail.com>
2019-12-03 20:19:44 +0000

    adds coverage to hard lockout policy attributes converter

--- adapter/api/src/test/java/uk/co/idv/api/lockout/policy/hard/HardLockoutPolicyAttributesConverterTest.java
+++ adapter/api/src/test/java/uk/co/idv/api/lockout/policy/hard/HardLockoutPolicyAttributesConverterTest.java
@@ -1,9 +1,11 @@
 package uk.co.idv.api.lockout.policy.hard;
 
 import org.junit.jupiter.api.Test;
+import uk.co.idv.api.lockout.policy.LockoutPolicyAttributes;
 import uk.co.idv.api.lockout.policy.LockoutPolicyAttributesMother;
 import uk.co.idv.api.lockout.policy.LockoutPolicyAttributesConverter;
 import uk.co.idv.domain.entities.lockout.policy.LockoutPolicy;
+import uk.co.idv.domain.entities.lockout.policy.LockoutPolicyMother;
 import uk.co.idv.domain.entities.lockout.policy.hard.HardLockoutPolicy;
 import uk.co.idv.domain.entities.lockout.policy.recordattempt.RecordAttemptStrategy;
 import uk.co.idv.domain.entities.lockout.policy.recordattempt.RecordAttemptStrategyFactory;
@@ -15,48 +17,110 @@ import static org.mockito.Mockito.mock;
 
 class HardLockoutPolicyAttributesConverterTest {
 
-    private final HardLockoutPolicyAttributes parameters = LockoutPolicyAttributesMother.hardLock();
-
     private final RecordAttemptStrategyFactory recordAttemptStrategyFactory = mock(RecordAttemptStrategyFactory.class);
 
     private final LockoutPolicyAttributesConverter converter = new HardLockoutPolicyAttributesConverter(recordAttemptStrategyFactory);
 
     @Test
-    void shouldPopulateIdOnPolicy() {
-        final LockoutPolicy policy = converter.toPolicy(parameters);
+    void shouldSupportHardLockoutType() {
+        assertThat(converter.supports(HardLockoutStateCalculator.TYPE)).isTrue();
+    }
 
-        assertThat(policy.getId()).isEqualTo(parameters.getId());
+    @Test
+    void shouldNotSupportAnyOtherLockoutType() {
+        assertThat(converter.supports("other-type")).isFalse();
+    }
+
+    @Test
+    void shouldPopulateIdOnPolicy() {
+        final HardLockoutPolicyAttributes attributes = LockoutPolicyAttributesMother.hardLock();
+
+        final LockoutPolicy policy = converter.toPolicy(attributes);
+
+        assertThat(policy.getId()).isEqualTo(attributes.getId());
     }
 
     @Test
     void shouldPopulateLockoutLevelOnPolicy() {
-        final LockoutPolicy policy = converter.toPolicy(parameters);
+        final HardLockoutPolicyAttributes attributes = LockoutPolicyAttributesMother.hardLock();
 
-        assertThat(policy.getLockoutLevel()).isEqualTo(parameters.getLockoutLevel());
+        final LockoutPolicy policy = converter.toPolicy(attributes);
+
+        assertThat(policy.getLockoutLevel()).isEqualTo(attributes.getLockoutLevel());
     }
 
     @Test
     void shouldPopulateRecordAttemptStrategyOnPolicy() {
+        final HardLockoutPolicyAttributes attributes = LockoutPolicyAttributesMother.hardLock();
         final RecordAttemptStrategy strategy = mock(RecordAttemptStrategy.class);
-        given(recordAttemptStrategyFactory.build(parameters.getRecordAttempts())).willReturn(strategy);
+        given(recordAttemptStrategyFactory.build(attributes.getRecordAttempts())).willReturn(strategy);
 
-        final LockoutPolicy policy = converter.toPolicy(parameters);
+        final LockoutPolicy policy = converter.toPolicy(attributes);
 
         assertThat(policy.getRecordAttemptStrategy()).isEqualTo(strategy);
     }
 
     @Test
     void shouldPopulateStateCalculatorOnPolicy() {
-        final LockoutPolicy policy = converter.toPolicy(parameters);
+        final HardLockoutPolicyAttributes attributes = LockoutPolicyAttributesMother.hardLock();
+
+        final LockoutPolicy policy = converter.toPolicy(attributes);
 
         assertThat(policy.getStateCalculator()).isInstanceOf(HardLockoutStateCalculator.class);
     }
 
     @Test
     void shouldPopulateMaxAttemptsOnPolicy() {
-        final HardLockoutPolicy policy = (HardLockoutPolicy) converter.toPolicy(parameters);
+        final HardLockoutPolicyAttributes attributes = LockoutPolicyAttributesMother.hardLock();
 
-        assertThat(policy.getMaxNumberOfAttempts()).isEqualTo(parameters.getMaxNumberOfAttempts());
+        final HardLockoutPolicy policy = (HardLockoutPolicy) converter.toPolicy(attributes);
+
+        assertThat(policy.getMaxNumberOfAttempts()).isEqualTo(attributes.getMaxNumberOfAttempts());
+    }
+
+    @Test
+    void shouldPopulateIdOnAttributes() {
+        final LockoutPolicy policy = LockoutPolicyMother.hardLockoutPolicy();
+
+        final LockoutPolicyAttributes attributes = converter.toAttributes(policy);
+
+        assertThat(attributes.getId()).isEqualTo(policy.getId());
+    }
+
+    @Test
+    void shouldPopulateLockoutLevelOnAttributes() {
+        final LockoutPolicy policy = LockoutPolicyMother.hardLockoutPolicy();
+
+        final LockoutPolicyAttributes attributes = converter.toAttributes(policy);
+
+        assertThat(attributes.getLockoutLevel()).isEqualTo(policy.getLockoutLevel());
+    }
+
+    @Test
+    void shouldPopulateRecordAttemptStrategyTypeOnAttributes() {
+        final LockoutPolicy policy = LockoutPolicyMother.hardLockoutPolicy();
+
+        final LockoutPolicyAttributes attributes = converter.toAttributes(policy);
+
+        assertThat(attributes.getRecordAttempts()).isEqualTo(policy.getRecordAttemptStrategyType());
+    }
+
+    @Test
+    void shouldPopulateLockoutTypeOnAttributes() {
+        final LockoutPolicy policy = LockoutPolicyMother.hardLockoutPolicy();
+
+        final LockoutPolicyAttributes attributes = converter.toAttributes(policy);
+
+        assertThat(attributes.getLockoutType()).isEqualTo(policy.getLockoutType());
+    }
+
+    @Test
+    void shouldPopulateMaxAttemptsOnAttributes() {
+        final HardLockoutPolicy policy = (HardLockoutPolicy) LockoutPolicyMother.hardLockoutPolicy();
+
+        final HardLockoutPolicyAttributes attributes = (HardLockoutPolicyAttributes) converter.toAttributes(policy);
+
+        assertThat(attributes.getMaxNumberOfAttempts()).isEqualTo(policy.getMaxNumberOfAttempts());
     }
 
 }
