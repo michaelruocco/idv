38cc5563c3880c58c02658ba3a2b96c5c0f35bf9
Michael Ruocco <michael.ruocco@hotmail.com>
2019-12-29 21:38:35 +0000

    removes unused import

--- adapter/api/src/main/java/uk/co/idv/api/verificationcontext/VerificationContextDocument.java
+++ adapter/api/src/main/java/uk/co/idv/api/verificationcontext/VerificationContextDocument.java
@@ -1,6 +1,5 @@
 package uk.co.idv.api.verificationcontext;
 
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import uk.co.idv.domain.entities.verificationcontext.VerificationContext;
 import uk.co.mruoc.jsonapi.ApiDataWithId;
 import uk.co.mruoc.jsonapi.ApiDocumentWithId;
