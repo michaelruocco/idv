ab654c10535b9c86b625ea07f1d65ddb49fa1543
Michael Ruocco <michael.ruocco@hotmail.com>
2020-01-04 19:42:05 +0000

    removes verification attempt mongo classes

--- adapter/db/mongo/src/main/java/uk/co/idv/repository/mongo/lockout/attempt/VerificationAttemptsRepository.java
+++
@@ -1,9 +0,0 @@
-package uk.co.idv.repository.mongo.lockout.attempt;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface VerificationAttemptsRepository extends MongoRepository<VerificationAttemptsDocument, String> {
-
-    // intentionally blank
-
-}
