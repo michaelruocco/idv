aaf2c446430c54a4a94a5119c74ec17d5a9e0790
Michael Ruocco <michael.ruocco@hotmail.com>
2020-01-26 21:45:07 +0000

    adds identity controller test

---
+++ app/spring-app/src/test/java/uk/co/idv/app/rest/identity/IdentityControllerTest.java
@@ -0,0 +1,60 @@
+package uk.co.idv.app.rest.identity;
+
+import org.junit.jupiter.api.Test;
+import uk.co.idv.api.identity.IdentityDocument;
+import uk.co.idv.domain.entities.identity.Identity;
+import uk.co.idv.domain.entities.identity.alias.Alias;
+import uk.co.idv.domain.entities.identity.alias.AliasFactory;
+import uk.co.idv.domain.entities.identity.alias.AliasesMother;
+import uk.co.idv.domain.entities.identity.alias.IdvId;
+import uk.co.idv.domain.usecases.identity.FakeIdentityService;
+import uk.co.idv.domain.usecases.identity.LoadIdentityRequest;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+class IdentityControllerTest {
+
+    private final AliasFactory aliasFactory = mock(AliasFactory.class);
+    private final Identity identity = new Identity(AliasesMother.aliases());
+
+    private final FakeIdentityService service = new FakeIdentityService(identity);
+
+    private final IdentityController controller = new IdentityController(aliasFactory, service);
+
+    @Test
+    void shouldBuildAliasAndPassInLoadIdentityRequest() {
+        final Alias alias = AliasesMother.debitCardNumber();
+        given(aliasFactory.build(alias.getType(), alias.getValue())).willReturn(alias);
+
+        controller.getIdentity(alias.getType(), alias.getValue());
+
+        final LoadIdentityRequest request = service.getLastLoadRequest();
+        assertThat(request.getAlias()).isEqualTo(alias);
+    }
+
+    @Test
+    void shouldBuildIdvIdAliasAndPassInLoadIdentityRequest() {
+        final UUID id = UUID.randomUUID();
+        final Alias idvId = new IdvId(id);
+        given(aliasFactory.build(IdvId.TYPE, id.toString())).willReturn(idvId);
+
+        controller.getIdentity(id);
+
+        final LoadIdentityRequest request = service.getLastLoadRequest();
+        assertThat(request.getAlias()).isEqualTo(idvId);
+    }
+
+    @Test
+    void shouldReturnLoadedIdentityInIdentityDocument() {
+        final UUID id = UUID.randomUUID();
+
+        final IdentityDocument document = controller.getIdentity(id);
+
+        assertThat(document.getAttributes()).isEqualTo(identity);
+    }
+
+}
