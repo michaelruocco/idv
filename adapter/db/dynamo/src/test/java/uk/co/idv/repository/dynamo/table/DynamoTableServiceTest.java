503eb13b45486a87beab4dc8612fb5aa2723794d
Michael Ruocco <michael.ruocco@hotmail.com>
2020-01-26 21:22:00 +0000

    adds test coverage for table service recreate method

--- adapter/db/dynamo/src/test/java/uk/co/idv/repository/dynamo/table/DynamoTableServiceTest.java
+++ adapter/db/dynamo/src/test/java/uk/co/idv/repository/dynamo/table/DynamoTableServiceTest.java
@@ -3,10 +3,13 @@ package uk.co.idv.repository.dynamo.table;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -41,6 +44,37 @@ public class DynamoTableServiceTest {
         service.getOrCreateTable(request);
 
         verify(creator, times(1)).create(request);
+    }
+
+    @Test
+    void shouldDeleteAndRecreateTableIfTableAlreadyCreated() {
+        final String tableName = "table-name";
+        final CreateTableRequest request = new CreateTableRequest()
+                .withTableName(tableName);
+        final Table expectedTable = mock(Table.class);
+        given(creator.create(request)).willReturn(expectedTable);
+        service.getOrCreateTable(request);
+
+        service.recreateTable(request);
+
+        final InOrder inOrder = Mockito.inOrder(expectedTable, creator);
+        inOrder.verify(creator).create(request);
+        inOrder.verify(expectedTable).delete();
+        inOrder.verify(creator).create(request);
+    }
+
+    @Test
+    void shouldNotDeleteIfTableDoesNotAlreadyExist() {
+        final String tableName = "table-name";
+        final CreateTableRequest request = new CreateTableRequest()
+                .withTableName(tableName);
+        final Table expectedTable = mock(Table.class);
+        given(creator.create(request)).willReturn(expectedTable);
+
+        service.recreateTable(request);
+
+        verify(expectedTable, never()).delete();
+        verify(creator).create(request);
     }
 
 }
