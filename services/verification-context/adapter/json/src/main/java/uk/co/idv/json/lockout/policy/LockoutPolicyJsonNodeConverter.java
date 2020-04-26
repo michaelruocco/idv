105d243b1b8a22ee5da42b1b205c482d628d5165
Michael Ruocco <michael.ruocco@hotmail.com>
2020-04-26 20:40:51 +0100

    removes unused methods

--- services/verification-context/adapter/json/src/main/java/uk/co/idv/json/lockout/policy/LockoutPolicyJsonNodeConverter.java
+++ services/verification-context/adapter/json/src/main/java/uk/co/idv/json/lockout/policy/LockoutPolicyJsonNodeConverter.java
@@ -1,30 +1,22 @@
 package uk.co.idv.json.lockout.policy;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import uk.co.idv.domain.entities.lockout.policy.LockoutLevel;
-import uk.co.idv.domain.entities.lockout.policy.LockoutPolicy;
 import uk.co.idv.domain.entities.lockout.policy.recordattempt.RecordAttemptStrategy;
 import uk.co.idv.utils.json.converter.jackson.JsonNodeConverter;
 
-import java.io.IOException;
+public class LockoutPolicyJsonNodeConverter {
 
-public interface LockoutPolicyJsonNodeConverter {
-
-    boolean supportsType(final String type);
-
-    LockoutPolicy toPolicy(final JsonNode node, final JsonParser parser, final DeserializationContext context) throws IOException;
-
-    static String toType(final JsonNode node) {
-        return node.get("type").asText();
+    private LockoutPolicyJsonNodeConverter() {
+        // utility class
     }
 
-    static LockoutLevel toLevel(final JsonNode node, final JsonParser parser) {
+    public static LockoutLevel toLevel(final JsonNode node, final JsonParser parser) {
         return JsonNodeConverter.toObject(node.get("level"), parser, LockoutLevel.class);
     }
 
-    static RecordAttemptStrategy toRecordAttempts(final JsonNode node, final JsonParser parser) {
+    public static RecordAttemptStrategy toRecordAttempts(final JsonNode node, final JsonParser parser) {
         return JsonNodeConverter.toObject(node.get("recordAttempts"), parser, RecordAttemptStrategy.class);
     }
 
